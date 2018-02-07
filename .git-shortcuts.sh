6a216350e5586265b664fa12d8afa04bae96b7a1
Amit Kumar Srivastava <amit.srivastava@moonfroglabs.com>
2018-02-07 13:31:20 +0530

    initial commit

---
+++ .git-shortcuts.sh
@@ -0,0 +1,37 @@
+# ----------------------
+# Git Aliases
+# ----------------------
+alias ga='git add'
+alias gaa='git add .'
+alias gaaa='git add -A'
+alias gb='git branch'
+alias gbd='git branch -d '
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gcom='git checkout master'
+alias gd='git diff'
+alias gda='git diff HEAD'
+alias gi='git init'
+alias gl='git log'
+alias glg='git log --graph --oneline --decorate --all'
+alias gld='git log --pretty=format:"%h %ad %s" --date=short --all'
+alias gm='git merge --no-ff'
+alias gp='git pull --rebase'
+alias gpo='git pull origin'
+alias gpom='git pull origin master'
+alias gs='git status'
+alias gss='git status -s'
+alias gst='git stash'
+alias gstl='git stash list'
+alias gstp='git stash pop'
+alias gstd='git stash drop'
+alias gconflicts='git diff --name-only --diff-filter=U'
+alias gpusho='git push origin'
+
+# ----------------------
+# Git Functions
+# ----------------------
+# Git log find by commit message
+function glf() { git log --all --grep="$1"; }
